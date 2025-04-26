1f9ae5556eac39675970a2ef2e3c0b85b761e359
zack5 <zack5@users.noreply.github.com>
2025-04-26 14:42:07 -0400

    Deletes context on end of game (prevents undoing to prev game state)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,19 +8,18 @@ import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 
 export default function App() {
-  return (
-    <>
-      <ContextNavigationProvider>
-          <BrowserRouter>
+    return (
+        <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/play/" element={<Navigate to="/play/LetterGrams" replace />} />
-              <Route path="/play/:letters" element={<Game />} />
-              <Route path="/about" element={<About />} />
-              <Route path="*" element={<NotFound />} />
-              </Routes>
-          </BrowserRouter>
-      </ContextNavigationProvider>
-    </>
-  )
+                <Route path="/" element={<Home />} />
+                <Route path="/play/" element={<Navigate to="/play/LetterGrams" replace />} />
+                <Route path="/play/:letters" element={
+                    <ContextNavigationProvider>
+                        <Game />
+                    </ContextNavigationProvider>} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
