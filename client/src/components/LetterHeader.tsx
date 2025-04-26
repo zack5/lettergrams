c02145f910660d41bd59a8ccc6c1114956513695
zack5 <zack5@users.noreply.github.com>
2025-04-26 14:07:12 -0400

    Fixes letterheader with new coloration changes

--- client/src/components/LetterHeader.tsx
+++ client/src/components/LetterHeader.tsx
@@ -1,8 +1,25 @@
+import { GRID_SIZE } from "../constants/Constants";
 import Letter from "./Letter";
 
-export default function LetterHeader({text} : {text: string}) {
+export default function LetterHeader({ text }: { text: string }) {
+    const backgroundWidth = Math.max(text.length, 12) * GRID_SIZE
+    const backgroundSize = `${backgroundWidth}px ${GRID_SIZE * 2}px`
     const elems = text.toUpperCase().split('').map((letter, index) => {
-        return <Letter key={index} letter={letter} />;
+        const backgroundPosition = `-${GRID_SIZE * index}px 0px`
+        return (
+            <Letter
+                key={index}
+                letter={letter}
+                props={{
+                    style: {
+                        width: GRID_SIZE,
+                        height: GRID_SIZE,
+                        backgroundPosition,
+                        backgroundSize,
+                    }
+                }}
+            />
+        );
     });
     return <div className="letter-header">{elems}</div>;
 }
